b863508048a1cd7a080942459a18dff49a3fbe3c
Pieter Koornhof <pieter@entelect.co.za>
2014-10-01 10:57:17 +0200

    Implement Home Controller

---
+++ SneakySignal/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+
+namespace SneakySignal.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            if (Request.Browser.IsMobileDevice)
+            {
+                RedirectToAction("Mobile");
+            }
+            RedirectToAction("Desktop");
+        }
+        
+        public ActionResult Desktop()
+        {
+            return View();
+        }
+        
+        public ActionResult Mobile()
+        {
+            return View();
+        }
+	}
+}
